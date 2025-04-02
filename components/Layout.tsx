5a747d98568dfa8e24d92b40a1ab2bd7046445ef
gastrodia <3025822868@qq.com>
2025-04-02 08:48:25 +0000

    feat: work init

---
+++ components/Layout.tsx
@@ -0,0 +1,14 @@
+export default function Layout() {
+  return (
+    <div class="chat-container flex flex-col max-w-7xl mx-auto bg-white rounded-xl shadow-md overflow-hidden">
+      <h1>111</h1>
+      <h1>111</h1>
+      <h1>111</h1>
+      <h1>111</h1>
+      <h1>111</h1>
+      <h1>111</h1>
+      <h1>111</h1>
+      <h1>111</h1>
+    </div>
+  );
+}
---
+++ routes/index.tsx
@@ -0,0 +1,8 @@
+import Layout from '../components/Layout.tsx'
+import Chat from '../components/Chat.tsx'
+export default function Home() {
+  return <>
+  <button type="button" class="btn btn-primary">DaisyUI 按钮</button>
+  <Chat/>
+  </>
+}
